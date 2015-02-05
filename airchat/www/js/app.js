3ae72b39f29e43fc0cdc77abc9ca908e12ce57ef
Manton Horton <mantonhorton@gmail.com>
2015-02-05 15:00:41 -0500

    change controller information

--- airchat/www/js/app.js
+++ airchat/www/js/app.js
@@ -53,7 +53,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'tab-adminlogin': {
           templateUrl: 'templates/tab-adminlogin.html',
-          controller: 'ChatsCtrl'
+          controller: 'AdminLoginCtrl'
         }
       }
     })
